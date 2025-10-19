b6a003ffc9df11acff26bbbc7fa6d326a54593ed
Mohamed Badr <badrm7103@gmail.com>
2025-10-19 19:37:57 +0300

    modify routing

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function AuthRedirect({ children }: { children: React.ReactNode }) {
     return <Navigate to={user.role === "admin" ? "/admin" : "/user"} replace />;
   }
 
-  return <AuthPage/>;
+  return <Navigate to="/" replace />;
 }
 
 // Root redirect component
