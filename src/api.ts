f38b1dbc7f4aaf86bf9b29cd2ba733f1ac95b304
Mohamed Badr <badrm7103@gmail.com>
2025-11-16 01:25:26 +0200

    export api_url

--- src/api.ts
+++ src/api.ts
@@ -9,7 +9,7 @@ import type {
 
 // export const API_URL = "https://calibration-tool-server.vercel.app/api"; //vercel api url
 
-const API_URL = "/api";  // Calls to the same deployment where frontend lives (serverless vercel feature)
+export const API_URL = "/api";  // Calls to the same deployment where frontend lives (serverless vercel feature)
 
 
 // 🔥 Token Management
